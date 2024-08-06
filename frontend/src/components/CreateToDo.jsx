d06747e06888028b691d3389e1c334d6e3b6a9c9
abhitiwari2511 <abhishek.tiwari@hmritm.ac.in>
2024-08-07 01:59:05 +0530

    frontend

--- frontend/src/components/CreateToDo.jsx
+++ frontend/src/components/CreateToDo.jsx
@@ -1,12 +1,15 @@
+import "../components/CreateToDo.css"
+
 export function CreateToDo() {
     return (
         <>
             <h1>Create a new To-Do</h1>
-            <form>
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" name="title" />
-                <label htmlFor="description">Description</label>
-                <input type="text" id="description" name="description" />
+            <form className="formInput">
+                <label className="label" htmlFor="title">Title</label>
+                <input className="input" type="text" id="title" name="title" />
+                <label className="label" htmlFor="description">Description</label>
+                <input className="descriptionInput" type="text" id="description" name="description" />
+                <br/>
                 <button type="submit">Create</button>
             </form>
         </>
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,9 @@
+export function Todos() {
+    return (
+        <>
+            <h1>Go for walk</h1>
+            <h2>We should go for a walk</h2>
+            <button>MArk as completed</button>
+        </>
+    )
+}
